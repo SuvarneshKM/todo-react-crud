3831a65dc6938d73af1b547fd4b685b37ef1ef54
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-26 22:41:48 +0530

    Working and simple Todo App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Drink water','Drink coffee','run 10km']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    console.log('👾','I am coming!!!');
+    setTodos([...todos, input])
+  }
+
   return (
     <div className="App">
       <h1>ToDo App 🚀 </h1>
-      <input />
-      <button>Add Todo</button>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
       <ul>
-        <li>Drink water</li>
-        <li>Drink coffee</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
