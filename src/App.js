97444eaaad31808d0cf6cfa484e3d78cc7ec891e
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-26 20:51:46 +0530

    Basic UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>ToDo App 🚀 </h1>
+      <input />
+      <button>Add Todo</button>
+      <ul>
+        <li>Drink water</li>
+        <li>Drink coffee</li>
+      </ul>
     </div>
   );
 }
