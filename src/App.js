f3967635a76e8de42be89a6fc109574ee8594956
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-28 20:26:41 +0530

    Alert Box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import React from 'react';
+import { Button, FormControl, InputLabel, Input, Snackbar } from '@material-ui/core';
 import { useState } from 'react';
 import './App.css';
 import Todo from './Todo';
+import MuiAlert from '@material-ui/lab/Alert';
+
 
 function App() {
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const [open, setOpen] = useState(false);
   const [todos, setTodos] = useState(['Drink water', 'Drink coffee', 'run 10km']);
   const [input, setInput] = useState('');
 
@@ -11,7 +19,15 @@ function App() {
     event.preventDefault();
     setTodos([...todos, input]);
     setInput('');
+    setOpen(true);
   }
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <div className="App">
@@ -22,6 +38,11 @@ function App() {
           <Input value={input} onChange={event => setInput(event.target.value)} />
         </FormControl>
         <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">Add Todo</Button>
+        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            Todo Added!
+          </Alert>
+        </Snackbar>
       </form>
       <ul>
         {todos.map(todo => (
