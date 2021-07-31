65beda072b01bdaaa2ef47df2ca2a03d1137832a
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-31 14:45:45 +0530

    add todo to db with timestamp

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Todo from './Todo';
 import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
   useEffect(() => {
-    db.collection('todos').onSnapshot(snapshot => {
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setTodos(snapshot.docs.map(doc => doc.data().todo))
     })
   }, []);
@@ -19,6 +20,7 @@ function App() {
 
     db.collection('todos').add({
       todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
 
     setTodos([...todos, input]);
